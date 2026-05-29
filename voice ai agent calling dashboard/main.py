e79f09cea75cdad27190d2b6db69fc9085001e10
ACER <ACER@LAPTOP-OAE8B2TE>
2026-05-29 12:16:32 +0300

    removed s3 and added sqs instead

--- voice ai agent calling dashboard/main.py
+++ voice ai agent calling dashboard/main.py
@@ -2,16 +2,19 @@ import boto3
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import os
 from dotenv import load_dotenv
-
+import openpyxl
+import pandas as pd
+import json
 load_dotenv()
 AWS_ACCESS_ID = os.getenv("AWS_ACCESS_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 app = FastAPI()
-S3_BUCKET = "excelsheetbucketmuditrajsade"
+#S3_BUCKET = "excelsheetbucketmuditrajsade"
+queueurl = "https://sqs.ap-south-1.amazonaws.com/633665344245/numbersqueue"
 S3_REGION = "ap-south-1"
 
-s3 = boto3.client(
-    "s3",
+sqs = boto3.client(
+    "sqs",
     region_name=S3_REGION,
     aws_access_key_id=AWS_ACCESS_ID,
     aws_secret_access_key=AWS_SECRET_KEY,
@@ -26,7 +29,11 @@ def upload(file: UploadFile = File(...)):
     if not file.filename.endswith((".xlsx", ".xls")):
         raise HTTPException(status_code=400, detail="Only Excel files are allowed")
     try:
-        s3.upload_fileobj(file.file, S3_BUCKET, file.filename)
-        return {"message": f"{file.filename} uploaded to S3 bucket {S3_BUCKET}"}
+        file = pd.read_excel(file.file)
+        first_column = file['Phone'].tolist()
+        for num in first_column:
+            print(num)
+            sqs.send_message(QueueUrl=queueurl, MessageBody=json.dumps({"numbers": str(num)}))
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=str(e))
